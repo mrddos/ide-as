2ff6ad56a29effbd1c74cc7542e4aa0d2c4f9713
lucas.eca.pucminas@gmail.com <lucas.eca.pucminas@gmail.com@7abbf4b6-418b-3b7e-21db-c12507c3cd0c>
2011-04-01 12:47:48 +0000

    Insercao de comentario

--- trunk/ideas/ideas.server.manager/Main.cs
+++ trunk/ideas/ideas.server.manager/Main.cs
@@ -7,6 +7,7 @@ namespace ideas.server.manager
 	{
 		public static void Main (string[] args)
 		{
+			// Inicia a aplicacao
 			Application.Init ();
 			MainWindow win = new MainWindow ();
 			win.Show ();

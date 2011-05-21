383cd07aab5f32045c31fad729c6b0f684b96500
luizcancado <luizcancado@7abbf4b6-418b-3b7e-21db-c12507c3cd0c>
2011-05-21 00:21:29 +0000

    Fixed WebService instatiation Added default Tag structure

---
+++ trunk/ideas/Ideas.Scada.Common/Source/Tags/Tag.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Ideas.Common.Tags
+{
+	public struct Tag
+	{
+		public string TagName;
+ 	    public string DataType;
+        public DateTime DateTimeUpdate;
+        public string ClientAccess;
+        public string EngUnits;
+        public string Description;
+	}
+}

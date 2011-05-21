3054765cd1bd05770b329a3173280574845f3f3a
luizcancado <luizcancado@7abbf4b6-418b-3b7e-21db-c12507c3cd0c>
2011-05-21 21:57:38 +0000

    Fixed bug that icons do not appear on rendering

--- ideas/Ideas.Scada.Server.Manager/gtk-gui/generated.cs
+++ ideas/Ideas.Scada.Server.Manager/gtk-gui/generated.cs
@@ -6,71 +6,22 @@ namespace Stetic
 	{
 		private static bool initialized;
 
-		internal static void Initialize (Gtk.Widget iconRenderer)
+		static internal void Initialize (Gtk.Widget iconRenderer)
 		{
 			if ((Stetic.Gui.initialized == false)) {
 				Stetic.Gui.initialized = true;
 				global::Gtk.IconFactory w1 = new global::Gtk.IconFactory ();
-				global::Gtk.IconSet w2 = new global::Gtk.IconSet (global::Stetic.IconLoader.LoadIcon (iconRenderer, "gtk-about", global::Gtk.IconSize.Menu));
-				w1.Add ("gtk-about", w2);
-				global::Gtk.IconSet w3 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/scada.png")));
-				w1.Add ("scada", w3);
-				global::Gtk.IconSet w4 = new global::Gtk.IconSet ();
-				global::Gtk.IconSource w5 = new global::Gtk.IconSource ();
-				w5.Pixbuf = new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/icon_16x16.png"));
-				w4.AddSource (w5);
-				global::Gtk.IconSource w6 = new global::Gtk.IconSource ();
-				w6.Pixbuf = new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/icon_24x24.png"));
-				w4.AddSource (w6);
-				global::Gtk.IconSource w7 = new global::Gtk.IconSource ();
-				w7.Pixbuf = new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/icon_32x32.png"));
-				w4.AddSource (w7);
-				w1.Add ("icon", w4);
-				global::Gtk.IconSet w8 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, ".\\Resources\\Icons\\Interface\\16\\webservice.png")));
-				w1.Add ("tagswebservice", w8);
-				global::Gtk.IconSet w9 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, ".\\Resources\\Icons\\Interface\\16\\database.png")));
-				w1.Add ("tagsdatabase", w9);
-				global::Gtk.IconSet w10 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, ".\\Resources\\Icons\\Interface\\16\\folder.png")));
-				w1.Add ("folder", w10);
+				global::Gtk.IconSet w2 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/scada.png")));
+				w1.Add ("scada", w2);
+				global::Gtk.IconSet w3 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/webservice.png")));
+				w1.Add ("tagswebservice", w3);
+				global::Gtk.IconSet w4 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/database.png")));
+				w1.Add ("tagsdatabase", w4);
+				global::Gtk.IconSet w5 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/folder.png")));
+				w1.Add ("folder", w5);
+				global::Gtk.IconSet w6 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Interface/16/screen.png")));
+				w1.Add ("screen", w6);
 				w1.AddDefault ();
-			}
-		}
-	}
-
-	internal class IconLoader
-	{
-		public static Gdk.Pixbuf LoadIcon (Gtk.Widget widget, string name, Gtk.IconSize size)
-		{
-			Gdk.Pixbuf res = widget.RenderIcon (name, size, null);
-			if ((res != null)) {
-				return res;
-			} else {
-				int sz;
-				int sy;
-				global::Gtk.Icon.SizeLookup (size, out sz, out sy);
-				try {
-					return Gtk.IconTheme.Default.LoadIcon (name, sz, 0);
-				} catch (System.Exception) {
-					if ((name != "gtk-missing-image")) {
-						return Stetic.IconLoader.LoadIcon (widget, "gtk-missing-image", size);
-					} else {
-						Gdk.Pixmap pmap = new Gdk.Pixmap (Gdk.Screen.Default.RootWindow, sz, sz);
-						Gdk.GC gc = new Gdk.GC (pmap);
-						gc.RgbFgColor = new Gdk.Color (255, 255, 255);
-						pmap.DrawRectangle (gc, true, 0, 0, sz, sz);
-						gc.RgbFgColor = new Gdk.Color (0, 0, 0);
-						pmap.DrawRectangle (gc, false, 0, 0, (sz - 1), (sz - 1));
-						gc.SetLineAttributes (3, Gdk.LineStyle.Solid, Gdk.CapStyle.Round, Gdk.JoinStyle.Round);
-						gc.RgbFgColor = new Gdk.Color (255, 0, 0);
-						pmap.DrawLine (gc, (sz / 4), (sz / 4), ((sz - 1) 
-                                        - (sz / 4)), ((sz - 1) 
-                                        - (sz / 4)));
-						pmap.DrawLine (gc, ((sz - 1) 
-                                        - (sz / 4)), (sz / 4), (sz / 4), ((sz - 1) 
-                                        - (sz / 4)));
-						return Gdk.Pixbuf.FromDrawable (pmap, pmap.Colormap, 0, 0, 0, 0, sz, sz);
-					}
-				}
 			}
 		}
 	}
@@ -78,6 +29,7 @@ namespace Stetic
 	internal class BinContainer
 	{
 		private Gtk.Widget child;
+
 		private Gtk.UIManager uimanager;
 
 		public static BinContainer Attach (Gtk.Bin bin)
@@ -119,8 +71,7 @@ namespace Stetic
 			if ((this.uimanager != null)) {
 				Gtk.Widget w;
 				w = this.child.Toplevel;
-				if (((w != null) 
-                            && typeof(Gtk.Window).IsInstanceOfType (w))) {
+				if (((w != null) && typeof(Gtk.Window).IsInstanceOfType (w))) {
 					((Gtk.Window)(w)).AddAccelGroup (this.uimanager.AccelGroup);
 					this.uimanager = null;
 				}

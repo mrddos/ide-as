7b9e28c7e2ee64928ecc041221ec66c5c51ccfcf
luizcancado <luizcancado@7abbf4b6-418b-3b7e-21db-c12507c3cd0c>
2011-05-21 00:20:09 +0000

    Added SQLite-based RTDB routine

--- ideas/Ideas.Scada.Common/Source/Tags/DataBase.cs
+++ ideas/Ideas.Scada.Common/Source/Tags/DataBase.cs
@@ -5,6 +5,7 @@ using Ideas.Common;
 using System.Collections.Generic;
 using Mono.Data.Sqlite;
 using System.Data;
+using LumenWorks.Framework.IO.Csv;
 
 namespace Ideas.Common.Tags
 {
@@ -22,17 +23,26 @@ namespace Ideas.Common.Tags
 		/// </summary>	
 		public DataBase ()
 		{
+					
+		}
+			
+		~DataBase ()
+		{
+			this.Stop();	
+		}
+		
+		public void Start()
+		{
 			dbcon = (IDbConnection) new SqliteConnection(connectionString);
 			dbcon.Open();
 			dbcmd = dbcon.CreateCommand();
 			
 			CreateDatabaseStructure();
 			
-			
-			
+			ReadSourceFile(); 
 		}
-			
-		~DataBase ()
+		
+		public void Stop()
 		{
 			if(dbcon != null)
 			{
@@ -45,7 +55,6 @@ namespace Ideas.Common.Tags
 				dbcon.Close();
 				dbcon = null;
 			}
-			
 		}
 		
 		void CreateDatabaseStructure ()
@@ -54,17 +63,94 @@ namespace Ideas.Common.Tags
 			sql += "CREATE TABLE tb"+ this.Name + " ( ";
             sql += "TagName varchar(50), ";
             sql += "DataType varchar(50), ";
-			sql += "DateTimeUpdate DATETIME, "
+			sql += "DateTimeUpdate DATETIME, ";
 			sql += "ClientAccess varchar(3), ";
 			sql += "EngUnits varchar(32), ";
 			sql += "Description varchar(255) ";
 			sql += " ); ";
 								
 			dbcmd.CommandText = sql;
-			
-			IDataReader reader = dbcmd.ExecuteNonQuery();
+			dbcmd.ExecuteNonQuery();
 		}
 
+		void ReadSourceFile ()
+		{
+			TextReader textReader = new StreamReader(this.FilePath);
+			CsvReader csvReader = new CsvReader(textReader, true, ',');
+					
+			while(csvReader.ReadNextRecord())
+			{
+				// Convert CSV record to Tag
+				Tag tag = new Tag();
+				tag.TagName = csvReader[0];
+				tag.DataType = csvReader[1];
+				tag.ClientAccess = csvReader[2];
+				tag.EngUnits = csvReader[3];
+				tag.Description = csvReader[4];
+				
+            	InsertTagToDatabase(tag);				
+			}
+		}
+
+		/// <summary>
+		/// Insert tag information in memory database
+		/// </summary>
+		/// <param name="tag">
+		/// A <see cref="Tag"/>
+		/// </param>
+		void InsertTagToDatabase(Tag tag)
+		{
+			string sql = "";
+			sql += "INSERT INTO tb"+ this.Name + " ( ";
+            sql += "TagName, ";
+            sql += "DataType, ";
+			sql += "DateTimeUpdate, ";
+			sql += "ClientAccess, ";
+			sql += "EngUnits, ";
+			sql += "Description ";
+			sql += " ) values ( ";
+			sql += "@TagName, ";
+            sql += "@DataType, ";
+			sql += "NULL, ";
+			sql += "@ClientAccess, ";
+			sql += "@EngUnits, ";
+			sql += "@Description ";
+			sql += " );";
+			
+			IDbDataParameter parTagName = dbcmd.CreateParameter();
+			parTagName.ParameterName = "TagName";
+			parTagName.DbType = DbType.String;
+			parTagName.Value = tag.TagName;
+			dbcmd.Parameters.Add(parTagName);
+			
+			IDbDataParameter parDataType = dbcmd.CreateParameter();
+			parDataType.ParameterName = "DataType";
+			parDataType.DbType = DbType.String;
+			parDataType.Value = tag.DataType;
+			dbcmd.Parameters.Add(parDataType);
+			
+			IDbDataParameter parClientAccess = dbcmd.CreateParameter();
+			parClientAccess.ParameterName = "ClientAccess";
+			parClientAccess.DbType = DbType.String;
+			parClientAccess.Value = tag.ClientAccess;
+			dbcmd.Parameters.Add(parClientAccess);
+			
+			IDbDataParameter parEngUnits = dbcmd.CreateParameter();
+			parEngUnits.ParameterName = "EngUnits";
+			parEngUnits.DbType = DbType.String;
+			parEngUnits.Value = tag.EngUnits;
+			dbcmd.Parameters.Add(parEngUnits);
+			
+			IDbDataParameter parDescription = dbcmd.CreateParameter();
+			parDescription.ParameterName = "Description";
+			parDescription.DbType = DbType.String;
+			parDescription.Value = tag.Description;
+			dbcmd.Parameters.Add(parDescription);
+			
+			dbcmd.CommandText = sql;
+			dbcmd.ExecuteNonQuery();
+		}
+		
 		#region P R O P E R T I E S
 		
 		public string Name 

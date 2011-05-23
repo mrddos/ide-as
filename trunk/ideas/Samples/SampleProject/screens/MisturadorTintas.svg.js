4f0509df5e74472317a8267336f4addc459491ff
luiz.cancado@gmail.com <luiz.cancado@gmail.com@7abbf4b6-418b-3b7e-21db-c12507c3cd0c>
2011-05-23 06:33:52 +0000

    Sample Application: Added client and server side scripts to screen "MisturadorTintas"

---
+++ trunk/ideas/Samples/SampleProject/screens/MisturadorTintas.svg.js
@@ -0,0 +1,15 @@
+/*
+	Ideas.Scada.Server - 2011
+	
+	Screen: tela
+	File: tela.svg.js (Client-side script)
+	Application: SampleApplication
+	Project: SampleProject
+	Author: Luiz Cançado
+	Description: A sample client-side script to demonstrate IDEAS application structure.
+*/
+
+function ClientSideMethod()
+{
+	// TODO
+}
